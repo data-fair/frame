838e781dce9c0340db5110c5763d248f4ef401c8
Alban Mouton <alban.mouton@gmail.com>
2025-01-22 12:47:59 +0100

    Pushed latest by gh-pages-multi

---
+++ latest/_nuxt/B_8FlUjr.js
@@ -0,0 +1,1 @@
+import{O as n,i as o,a5 as i}from"./Bsgdp2ni.js";const r=n({elevation:{type:[Number,String],validator(e){const t=parseInt(e);return!isNaN(t)&&t>=0&&t<=24}}},"elevation");function u(e){return{elevationClasses:o(()=>{const s=i(e)?e.value:e.elevation,a=[];return s==null||a.push(`elevation-${s}`),a})}}export{r as m,u};
