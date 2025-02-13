6edebe9516d0dcb4aea5ceaf6ef9db456f77f89e
Alban Mouton <alban.mouton@gmail.com>
2025-02-13 11:38:51 +0100

    Pushed main by gh-pages-multi

--- main/_nuxt/DUUClUX-.js
+++
@@ -1,1 +0,0 @@
-import{ag as t,o as e,i as s,aI as n}from"./E06ptF_r.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
