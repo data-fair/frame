6f6479a5ca7280f07063f3179608a2466b2f4d17
Alban Mouton <alban.mouton@gmail.com>
2025-12-13 15:55:07 +0100

    fix: make usePatternUrl return resolved url it not in iframe

--- lib/vue-router/use-parent-url.ts
+++ lib/vue-router/use-parent-url.ts
@@ -1,7 +1,7 @@
 import { onScopeDispose, type MaybeRefOrGetter } from 'vue'
 import type { RouteLocationRaw, Router } from 'vue-router'
 import { useVueRouterDFrameContent } from './d-frame-content.js'
-import { toValue, ref, watch } from 'vue'
+import { toValue, ref, watch, computed } from 'vue'
 import inIframe from '../utils/in-iframe.js'
 import type DFrameContent from '../DFrameContent.js'
 
@@ -10,16 +10,18 @@ export const useParentUrl = (
   router: Router,
   dFrameContent?: DFrameContent
 ) => {
-  const parentUrl = ref<string | undefined>()
-  if (!inIframe) return parentUrl
+  if (!inIframe) {
+    return computed(() => router.resolve(toValue(location)).href)
+  }
 
   dFrameContent = dFrameContent ?? useVueRouterDFrameContent()
   let resolved: string | null = null
   let listener: null | ((result: string) => void) = null
+  const parentUrl = ref<string | null>(null)
 
   watch(() => toValue(location), (to) => {
     if (listener && resolved) dFrameContent.removeParentUrlListener(resolved, listener)
-    parentUrl.value = undefined
+    parentUrl.value = null
     resolved = router.resolve(to).href
     listener = (result: string) => { parentUrl.value = result }
     dFrameContent.addParentUrlListener(resolved, listener)
